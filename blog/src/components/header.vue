c1228597c014d50879cad631b03e114ad23465b5
shirley.hsieh <saps920030@gmail.com>
2018-12-12 23:27:58 +0800

    add bootstrap-vue & update header style

--- blog/src/components/header.vue
+++ blog/src/components/header.vue
@@ -1,22 +1,30 @@
 <template>
-  <nav>
-      <li><router-link to='/' exact>Blog</router-link></li>
-      <li><router-link to='/list' exact>Blog List</router-link></li>
-      <li><router-link to='/add' exact>Blog Add</router-link></li>
-  </nav>
+  <b-navbar toggleable="md" type="dark" variant="warning">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="/">MindPost</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item href="/add">New Post</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
-
 export default {
- 
-  data () {
-    
-  }
-}
+  data() {}
+};
 </script>
 
 <style>
-
-
 </style>
--- blog/src/main.js
+++ blog/src/main.js
@@ -4,8 +4,11 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Routes from './routes'
 import Header from './components/header.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
+Vue.use(BootstrapVue);
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
